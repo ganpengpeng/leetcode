0279ee7979dd8859fb6a69f64e4feca564a666a6
ganpengpeng <ganpeng@outlook.com>
2019-04-06 23:29:00 +0800

    update

--- src/main/java/Solution687.java
+++ src/main/java/Solution687.java
@@ -1,2 +1,26 @@
 public class Solution687 {
+    int max = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null)
+            return 0;
+        int len = helper(root, root.val);
+        if (root.left != null)
+            len = Math.max(len, longestUnivaluePath(root.left));
+        if (root.right != null)
+            len = Math.max(len, longestUnivaluePath(root.right));
+        max = Math.max(max, len);
+        return max;
+    }
+
+    public int helper(TreeNode root, int v) {
+        if (root == null || root.val != v)
+            return -1;
+
+        int l = helper(root.left, v);
+        int r = helper(root.right, v);
+        if (l + r + 2 > max)
+            max = l + r + 2;
+        return Math.max(l, r) + 1;
+    }
 }
